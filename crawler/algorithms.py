077f093d44e72a37cddef0699c9b7baf3ae510f5
Schubert <schubertyi@gmail.com>
2020-06-15 15:20:44 -0300

    Removed requests functions

--- crawler/algorithms.py
+++ crawler/algorithms.py
@@ -3,7 +3,9 @@ import re
 import requests
 
 from bs4 import BeautifulSoup
-from time import sleep,localtime,strftime
+from time import sleep,localtime
+
+from crawler.requester import Requester
 
 class Crawler:
 	"""
@@ -14,34 +16,12 @@ class Crawler:
 		self.__sarcasm = sarcasm
 		self.__as_archived = as_archived
 
-		self.__htmls = []
-		self.__pages = None
-		self.__raw_data = []
-		self.__dataframe = pd.DataFrame(columns=["article_link","headline","is_sarcastic","text"])
+		self.__requests = list()
+		self.__raw_data = list()
+		self.__dataframe = pd.DataFrame(columns=["is_sarcastic","article_link","headline","text"])
 
 
-	def get_one_request(self, page_url):
-		"""Get one html request.
-
-		Returns
-		-------
-		requests.models.Response
-			Request related to a specific page.
-		"""
-		error = None
-
-		while error != False:
-			error = False
-			try:
-				html = requests.get(page_url, headers=self.__header)
-			except requests.RequestException:
-				print("([!] Error occurred, trying again the page {0} after 5 seconds".format(page_url))
-				sleep(5)
-				error = True
-		return html
-
-
-	def set_pages(self, html_class, re_string, start, end, element="a", shorter=0):
+	def set_requests(self, html_class, regex, remove, element="a", shorter=0):
 		"""Find the last page of a website.
 		e.g. "Pages 1, 2, ..., 34, 35" <- Last page here is 35;
 		Or find the strings rellated to the archived data.
@@ -65,52 +45,26 @@ class Crawler:
 			If it's archived style page, set how much to remove from the url to get the
 			pages url list. If necessary.
 		"""
-		find_pages = re.compile(re_string)
-		html = True
+		find_pages = re.compile(regex)
+		urls = list()
 
-		while html == True:
-			html = self.get_one_request(self.__url + ("1", "")[self.__as_archived])
+		html = Requester.get_one_request(self.__url + ("1", "")[self.__as_archived])
 		bs = BeautifulSoup(html.text, "html.parser")
 		element = str(bs.find_all(element, class_=html_class))
 
 		if self.__as_archived == True:
-			pages = []
-			for archive in find_pages.finditer(element):
-				pages.append(str(self.__url[:-shorter]) + element[archive.start()+start:archive.end()-end])
+			for page in find_pages.finditer(element):
+				urls.append(str(self.__url[:-shorter]) + element[page.start()+remove[0]:page.end()-remove[1]])
 		else:
 			for page in find_pages.finditer(element):
-				pages = int(element[page.start()+start:page.end()-end])
+				pages = int(element[page.start()+remove[0]:page.end()-remove[1]]) + 1
+			for page in range(1, pages):
+				urls.append(self.__url + str(page))
 
-		self.__pages = pages
-
-
-	def get_html_requests(self):
-		htmls = []
-		"""Get all requests from a ammount of URLs.
-
-		Returns
-		-------
-		list
-			All pages requested.
-
-		Note
-		----
-		May occurs an infinite loop if any type of internet interference happens.
-		"""
-		print("[+] Requesting all pages (it might take a while) from:\n\t\'{0}\'".format(self.__url))
-
-		if self.__as_archived == True:
-			for arch in self.__pages:
-				htmls.append(self.get_one_request(arch))
-		else:
-			for page_num in range(1, self.__pages):
-				page_url = self.__url + str(page_num)
-				htmls.append(self.get_one_request(page_url))
-				
-		return htmls
+		self.__requests = Requester(urls, 10).get_requests()
 
 
-	def set_raw_data(self, html_class, re_string, element="div"):
+	def set_raw_data(self, html_class, regex, element="div"):
 		"""Find some specific part of an HTML class.
 
 		Parameters
@@ -122,18 +76,18 @@ class Crawler:
 		element : str, default "div"
 			Element of the HTML to be founded.
 		"""
-		find_element = re.compile(re_string)
+		find_element = re.compile(regex)
 
-		for html in self.get_html_requests():
-			bs = BeautifulSoup(html.text, "html.parser")
+		for request in self.__requests:
+			bs = BeautifulSoup(request.text, "html.parser")
 			raw = str(bs.find_all(element, class_=html_class))
 			for text in find_element.finditer(raw):
 				self.__raw_data.append(raw[text.start():text.end()])
 
-		print("[+] Total of {0} raw data collected".format(len(self.__raw_data)))
+		print("[+] Total of {0:04d} raw data collected".format(len(self.__raw_data)))
 
 
-	def set_data_frame(self, re_link, re_title, rm_start, rm_end, url_prefix=0):
+	def set_data_frame(self, regex, remove, url_prefix=0):
 		"""Find a specific data of links and title information from a list of
 		HTML specific data.
 
@@ -157,18 +111,19 @@ class Crawler:
 			If there's anything to add in the begging of the url string to change in
 			the columns of "link" in the dataset.
 		"""
-		find_link = re.compile(re_link)
-		find_title = re.compile(re_title)
+		find_link = re.compile(regex[0])
+		find_title = re.compile(regex[1])
 
 		for data in self.__raw_data:
 			data_list = [[],[],[],[]]
+			data_list[0] = self.__sarcasm
 			for tmp in find_link.finditer(data):
-				data_list[0] = self.__url[:url_prefix] + data[tmp.start()+rm_start[0]:tmp.end()+rm_end[0]]
+				data_list[1] = self.__url[:url_prefix] + data[tmp.start()+remove[0][0]:tmp.end()+remove[0][1]]
 			for tmp in find_title.finditer(data):
-				data_list[1] = data[tmp.start()+rm_start[1]:tmp.end()+rm_end[1]]
-			if data_list[1] == "" or data_list[3] == "":
+				data_list[2] = data[tmp.start()+remove[1][0]:tmp.end()+remove[1][1]]
+			if data_list[2] == "" or data_list[3] == "":
 				continue
-			data_list[2] = self.__sarcasm
+			data_list[3] = ""
 			self.__dataframe.loc[len(self.__dataframe)] = data_list
 
 		print("[+] Dataframe completed")
