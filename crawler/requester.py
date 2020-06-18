6e460d8b806604febd71f50bf9ecbb4795614102
Schubert <schubertyi@gmail.com>
2020-06-17 23:17:41 -0300

    Added list urls list size deppending on number of specified threads

--- crawler/requester.py
+++ crawler/requester.py
@@ -1,66 +1,80 @@
 import requests
 
 from time import sleep
-from queue import Queue
 from threading import Thread
 
 class Requester:
-    __header = {'user-agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0"}
+    header = {'user-agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0"}
 
-    def __init__(self, url_list, __list_size=25):
-        self.__url_list = url_list
-        self.__list_size = 25
-        
-        self.__threads = list()
+    def __init__(self, url_list, num_threads = 4):
+        self.__len_urls = len(url_list)
+        self.__urls_list = list()
+
+        size = int(len(url_list)/num_threads) + 1
+        for i in range(0, len(url_list), size):
+            self.__urls_list.append(url_list[i:i+size])
+            
         self.__requests = list()
+        self.__progress = 0
 
-        self.set_requests()
+        self.__set_requests()
 
 
-    def set_requests(self):
-        progress = 0
+    def __set_requests(self):
+        # Split the list of URLs to make the requests into multiple threads
         indexer = 0
-        for i in range(0, len(self.__url_list), self.__list_size):
+        threads = list()
+        for urls in self.__urls_list:
             thread = Thread(
-                    target=self.get_urls_requests,
-                    args=(self.__url_list[i:i+self.__list_size], indexer)
+                    target=self.__set_request_list,
+                    args=(urls, indexer)
                     )
             thread.start()
             indexer += 1
-            self.__threads.append(thread)
+            threads.append(thread)
 
-        print("{0:03d}/{1:03d} threads completed".format(progress,len(self.__threads)), end='\r')
-        for thread in self.__threads:
+        # Wait for the threads to complete
+        for thread in threads:
             thread.join()
-            progress += 1
-            print("{0:03d}/{1:03d} threads completed".format(progress,len(self.__threads)), end='\r')
             sleep(0.01)
+            del thread
         print()
 
-        requests_sorted = sorted(self.__requests, key = lambda x: x[0])
+        # Sort the requests in one list only in the same order they were requested
+        requests_sorted = sorted(self.__requests, key=lambda x: x[0])
         self.__requests.clear()
-
         for requests in requests_sorted:
             for request in requests[1]:
                 self.__requests.append(request)
-            
 
-    def get_urls_requests(self, urls, index):
+
+
+    def __set_request_list(self, urls, index):
         requests = (index, list())
+
         for url in urls:
             request = self.get_one_request(url)
             if request == None:
                 continue
             requests[1].append(request)
+            self.__progress()
         self.__requests.append(requests)
-    
-    
-    def get_one_request(self, url):
-        timeout = True
 
-        while timeout == True:
+
+    def __progress(self):
+        self.__progress += 1
+        print("[+] {0:05d}/{1:05d} requests completed.".format(
+                        self.__progress[0],
+                        self.__len_urls), end='\r')
+
+
+    @staticmethod
+    def get_one_request(url):
+        timeout = None
+
+        while timeout != False:
             try:
-                request = requests.get(url, headers=self.__header, timeout=5)
+                request = requests.get(url, headers=Requester.header, timeout=5)
                 timeout = False
             except (requests.ConnectionError,requests.ReadTimeout):
                 timeout = True
@@ -72,4 +86,4 @@ class Requester:
 
 
     def get_requests(self):
-        return self.__requests 
+        return self.__requests
