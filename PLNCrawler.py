9ca44657d5951e4e30d846762474b4d2717a7526
Schubert <schubertyi@gmail.com>
2020-06-17 23:13:10 -0300

    TODO: create controller

--- PLNCrawler.py
+++ PLNCrawler.py
@@ -1,7 +1,15 @@
-from crawler.usage import options
+from sys import version_info
 
-def main():
-	options()
+from crawler.sites import Sites
+
+if version_info[0] < 3:
+	raise Exception("PLNCrawler requires at least python3.8")
+if version_info[0] < 3 and version_info[1] < 8:
+	raise Exception("PLNCrawler requires at least python3.8")
+
+def _main():
+	Sites()
+
 
 if __name__ == '__main__':
-	main()
+	_main()
