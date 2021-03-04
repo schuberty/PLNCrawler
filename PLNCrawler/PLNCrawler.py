001b285bd2945124f3f00364efcbfa4d6da3da4e
Schuberty <schubertyi@gmail.com>
2021-03-04 19:41:04 -0300

    Changed required python version to 3.9

--- PLNCrawler/PLNCrawler.py
+++ PLNCrawler/PLNCrawler.py
@@ -1,11 +1,11 @@
 from sys import version_info
 
-from crawler.sites import Sites
+from .crawler.sites import Sites
 
 if version_info[0] < 3:
-	raise Exception("PLNCrawler requires at least python3.8")
+	raise Exception("PLNCrawler requires at least python3.9")
 if version_info[0] < 3 and version_info[1] < 8:
-	raise Exception("PLNCrawler requires at least python3.8")
+	raise Exception("PLNCrawler requires at least python3.9")
 
 def _main():
 	Sites()
